a6292a93bd23c487a4d22b099191de1946d1b94c
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-08-06 15:03:47 +0300

    Refactored top story items to its own dedicated component

--- src/TopStories.js
+++ src/TopStories.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
-import {Feed, Icon, Container, Label, Placeholder} from 'semantic-ui-react';
-import moment from 'moment';
-import {Link} from 'react-router-dom';
+import {Container, Placeholder} from 'semantic-ui-react';
 import axios from './plugins/axios';
+import Item from './Item';
 
 export default class TopStories extends Component {
   state = {
@@ -45,35 +44,7 @@ export default class TopStories extends Component {
                 </Placeholder>
               </div>
             )) : items.map((item) => (
-              <Feed key={item.id} size='large'>
-                <Feed.Event>
-                  <Feed.Label>
-                    <Label style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', width: 50, height: 50, borderRadius: 25 }}>
-                      {item.score}
-                    </Label>
-                  </Feed.Label>
-                  <Feed.Content>
-                    <Feed.Summary>
-                      <a href={item.url}>{item.title}</a>
-                    </Feed.Summary>
-                    <Feed.Meta style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start' }}>
-                      <Feed.Label style={{ borderRight: '1px solid rgba(0,0,0,.4)', paddingRight: 10 }}>
-                        by
-                        &nbsp;
-                        <Link to="#">{item.by}</Link>
-                        &nbsp;
-                        {moment.unix(item.time).fromNow()}
-                      </Feed.Label>
-                      <Feed.Like>
-                        <Link to={`/items/${item.id}`}>
-                          <Icon name='comments outline' />
-                          {item.descendants} comments
-                        </Link>
-                      </Feed.Like>
-                    </Feed.Meta>
-                  </Feed.Content>
-                </Feed.Event>
-              </Feed>
+              <Item key={item.id} item={item} />
             ))
           }
         </Container>
@@ -81,6 +52,3 @@ export default class TopStories extends Component {
     );
   }
 }
-
-
-//     "time": 1565049059,
