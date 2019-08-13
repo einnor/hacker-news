8587474fc951ac77442f36471113ad9ee2f6ec15
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-08-13 12:21:17 +0300

    Unescape and sanitize html

--- src/CommentItem.js
+++ src/CommentItem.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import {Comment} from 'semantic-ui-react';
 import moment from 'moment';
+import dompurify from 'dompurify';
 import Comments from './Comments';
 
-const CommentItem = ({comment}) => (
-  <React.Fragment>
-    <Comment>
-      <Comment.Avatar src="https://via.placeholder.com/50" />
-      <Comment.Content>
-        <Comment.Author as='a' style={{ textTransform: 'capitalize' }}>{comment.by}</Comment.Author>
-        <Comment.Metadata>
-          <div>{moment.unix(comment.time).fromNow()}</div>
-        </Comment.Metadata>
-        <Comment.Text>{comment.text}{comment.kids}</Comment.Text>
-      </Comment.Content>
+const CommentItem = ({comment}) => {
+  const sanitizer = dompurify.sanitize;
+  return (
+    <React.Fragment>
+      <Comment>
+        <Comment.Avatar src="https://via.placeholder.com/50" />
+        <Comment.Content>
+          <Comment.Author as='a' style={{ textTransform: 'capitalize' }}>{comment.by}</Comment.Author>
+          <Comment.Metadata>
+            <div>{moment.unix(comment.time).fromNow()}</div>
+          </Comment.Metadata>
+          <Comment.Text>
+            <div dangerouslySetInnerHTML={{__html: sanitizer(comment.text)}} />
+          </Comment.Text>
+        </Comment.Content>
 
-      {/* Nested comments */}
-      {
-        comment.kids && comment.kids.length ? (
-          <Comment.Group style={{ maxWidth: '100%' }}>
-            <Comments kids={comment.kids} />
-          </Comment.Group>
-        ) : null
-      }
-    </Comment>
-  </React.Fragment>
-);
+        {/* Nested comments */}
+        {
+          comment.kids && comment.kids.length ? (
+            <Comment.Group style={{ maxWidth: '100%' }}>
+              <Comments kids={comment.kids} />
+            </Comment.Group>
+          ) : null
+        }
+      </Comment>
+    </React.Fragment>
+  );
+};
 
 export default CommentItem;
