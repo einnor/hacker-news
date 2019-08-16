77ce7a6ddad811f29313e1d3c803d845fc760e4f
VivianDoreen <nabulovivian2014@gmail.com>
2019-08-16 20:16:17 +0300

    implement filter for ask page

---
+++ src/Sort.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Dropdown } from 'semantic-ui-react';
+
+const options = [
+  {
+    key: 'asc',
+    text: 'Asc',
+    value: 'asc'
+  },
+  {
+    key: 'desc',
+    text: 'desc',
+    value: 'desc'
+  }
+];
+
+const Sort = ({ onSortChange }) => (
+  <span>
+    <Dropdown
+      inline
+      options={options}
+      defaultValue={options[1].value}
+      onChange={onSortChange}
+    />
+  </span>
+);
+
+export default Sort;
