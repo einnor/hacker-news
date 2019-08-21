a5fc5bc77223808eef4c30c15f1c3a77afaee738
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-08-21 09:21:05 +0300

    Added selectors for top stories

---
+++ src/containers/TopStories/store/selectors.js
@@ -0,0 +1,4 @@
+export const getIsLoading = ({topStories}) => topStories.isLoading;
+export const getIds = ({topStories}) => topStories.ids;
+export const getItems = ({topStories}) => topStories.items;
+export const getError = ({topStories}) => topStories.error;
