96c41f527b82a8a7786de251a13c27724c2f7f5a
Ronnie Nyaga <ronnie@livefeather.com>
2019-08-28 09:39:54 +0300

    Added selectors for users

---
+++ src/containers/User/store/selectors.js
@@ -0,0 +1,6 @@
+export const getIsLoading = ({ user }) => user.isLoading;
+export const getId = ({ user }) => user.id;
+export const getKarma = ({ user }) => user.karma;
+export const getCreated = ({ user }) => user.created;
+export const getSubmitted = ({ user }) => user.submitted;
+export const getError = ({ user }) => user.error;
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import topStories from '../containers/TopStories/store/reducer';
 import topAsks from '../containers/TopAsks/store/reducers';
+import user from '../containers/User/store/reducer';
 // Add more reducers e,g Top Asks, User, Story
 
 const rootReducer = combineReducers({
   topStories,
-  topAsks
+  topAsks,
+  user,
 });
 
 export default rootReducer;
